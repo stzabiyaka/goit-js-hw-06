309fd7e21c852f3ed73126ac17bf6177f05ef25c
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2022-05-08 19:38:48 +0300

    Complete task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const makeiIngredientsItems = ingredientsArray => {
+  return ingredientsArray.map(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    item.classList.add('item');
+    return item;
+  });
+};
+
+const ingredientsItems = makeiIngredientsItems(ingredients);
+ingredientsList.append(...ingredientsItems);
